bc9b2bb7e91c74434596575fc0f815dd8def965f
nann6665 <nann6665@kea.stud.dk>
2022-08-26 08:54:24 +0200

    test

---
+++ src/main/java/com/example/helloworkazuretest/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.example.helloworkazuretest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/")
+    public String index(){
+        return "Hello World";
+    }
+
+}
---
+++ src/test/java/com/example/helloworkazuretest/HelloworkazuretestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworkazuretest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworkazuretestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
